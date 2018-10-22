817935748ec2a6f28dc43f1683bfb0ef3dacda18
hoangnv0901 <hoang.nguyenvan2107@gmail.com>
2018-10-22 10:36:32 +0700

    add application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # trả về tiêu đề đầy đủ trên cơ sở mỗi trang
+    def full_title(page_title = '')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.nil?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
